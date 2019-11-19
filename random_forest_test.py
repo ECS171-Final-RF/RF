ff59a8f66f42d31a484fb647933b874d08a2095d
jingwei-wan <56853293+jingwei-wan@users.noreply.github.com>
2019-11-18 20:54:00 -0800

    Add files via upload

--- random_forest_test.py
+++ random_forest_test.py
@@ -1,83 +1,78 @@
 import numpy as np
 import pandas as pd
+import re
 
 import matplotlib.pyplot as plt
-
+import nltk
+# nltk.download('wordnet')
 from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 
 
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
-from sklearn.svm import SVC
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report, confusion_matrix, precision_recall_curve
 
 np.random.seed(1)
 
-# Load the data
+# ====================================== Load the data =============================================================
 data = pd.read_csv('spam.csv', usecols = [0,1], encoding='latin-1')
 data.columns = ['label', 'text']
-print("The data has", data.shape[0], "row(s) and ", data.shape[1], "column(s)")
-print(data.head(), '\n')
 
+# ========================= Customized normalization of text developed by Kenneth ===================================
+total_stopwords = set([word.replace("'",'') for word in stopwords.words('english')])
+lemma = WordNetLemmatizer()
+def preprocess_text(text):
+    text = text.lower() # Convert to lowercase
+    text = text.replace("'",'') #
+    text = re.sub('[^a-zA-Z]',' ',text)
+    words = text.split()
+    words = [lemma.lemmatize(word) for word in words if (word not in total_stopwords) and (len(word)>1)] # Remove stop words
+    text = " ".join(words)
+    return text
 
-# Get a word count and unique word count of the text
-print(data.groupby('label').describe())
+data['text'] = data['text'].apply(preprocess_text)
 
-
-# print(data.groupby('label').describe())
-
-# data['length'] = data.text.str.len()
-# data['cat'] = data['label'].map({'ham' : 1, 'spam' : 0})
-# print(data)
-
-stop_words = set(stopwords.words('english'))
-
-##remove punc and stop
-# def remove_punc_stop(text):
-#     sen = [i for i in text if i not in string.punctuation]
-#     word = "".join(sen).split()
-#     word = [i.lower() for i in word if i.lower() not in stopwords.words("english")]
-#     word = " ".join(word)
-#     return word
-#
-# data['cleaned text'] = data.text.apply(remove_punc_stop)
-# print(data['cleaned text'])
-
-
-##counting the occurrences of tokens in each document.
+# ========================= features and labels definition ==========================================================
 vectorizer = CountVectorizer()
 x = vectorizer.fit_transform(data['text']).toarray()
 
-print(x)
 le = LabelEncoder()
 y = le.fit_transform(data['label'])
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.20, shuffle = True)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.15, shuffle = False)
 
-from sklearn.ensemble import RandomForestClassifier
+# ========================= Random Forest classier starts ===========================================================
 classifier=RandomForestClassifier(n_estimators=15,criterion='entropy')
 classifier.fit(x_train,y_train)
-predRF=classifier.predict(x_test)
-# for i in range(len(x_test)):
-#     print(x_test[i], predRF[i])
-from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score
-print('Accuracy score: {}'.format(accuracy_score(y_test, predRF)))
-print('Precision score: {}'.format(precision_score(y_test, predRF)))
-print('Recall score: {}'.format(recall_score(y_test, predRF)))
-print('F1 score: {}'.format(f1_score(y_test, predRF)))
+y_pred=classifier.predict(x_test)
 
+# ========================= precision_recall_curve plotting =========================================================
+pr, rc, thresholds = precision_recall_curve(y_test, y_pred)
+# Figure 1: precision, recall vs thresholds
+plt.figure(1)
+plt.plot(thresholds, pr[1:])
+plt.plot(thresholds, rc[1:])
+plt.xlabel('thresholds')
+plt.legend(['precision', 'recall'])
+crossover_index = np.max(np.where(pr <= rc))
+crossover_cutoff = thresholds[crossover_index]
+crossover_recall = rc[crossover_index]
 
-##To avoid these potential discrepancies it suffices to divide the number of occurrences
-##of each word in a document by the total number of words in the document:
-##these new features are called tf for Term Frequencies.
-# vectorizer2 = TfidfVectorizer()
-# data_X2 = vectorizer2.fit_transform(data['text'])
-#
-dictionary = np.array(vectorizer.get_feature_names())
-print(dictionary)
-# dictionary2 = np.array(vectorizer2.get_feature_names())
-#
+# Figure 2: ROC
+plt.figure(2)
+plt.plot(rc, pr, linestyle='--')
+plt.xlabel('Recall')
+plt.ylabel('Precision')
 
+# ========================== classification report and Confusion matrix ============================================
+print(classification_report(y_test, y_pred > crossover_cutoff))
+
+m_confusion_test = confusion_matrix(y_test, y_pred > crossover_cutoff)
+print(pd.DataFrame(data = m_confusion_test, columns = ['Predicted 0', 'Predicted 1'],
+            index = ['Actual 0', 'Actual 1']))
+
+# ==================================================================================================================
+plt.show()
