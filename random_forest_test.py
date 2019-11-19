d47e74d1548a5e7618be28f3a0e47c25ec8fafea
jingwei-wan <56853293+jingwei-wan@users.noreply.github.com>
2019-11-19 13:31:20 -0800

    Add files via upload

--- random_forest_test.py
+++ random_forest_test.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 import re
-
 import matplotlib.pyplot as plt
 import nltk
 # nltk.download('wordnet')
@@ -13,7 +12,7 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report, confusion_matrix, precision_recall_curve
+from sklearn.metrics import classification_report, confusion_matrix, precision_recall_curve, roc_auc_score
 
 np.random.seed(1)
 
@@ -45,12 +44,14 @@ y = le.fit_transform(data['label'])
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.15, shuffle = False)
 
 # ========================= Random Forest classier starts ===========================================================
-classifier=RandomForestClassifier(n_estimators=15,criterion='entropy')
+classifier = RandomForestClassifier(n_estimators=15,criterion='entropy')
 classifier.fit(x_train,y_train)
-y_pred=classifier.predict(x_test)
+y_pred_rt = classifier.fit(x_train, y_train).predict_proba(x_test)
+y_pred = classifier.predict(x_test)
 
 # ========================= precision_recall_curve plotting =========================================================
-pr, rc, thresholds = precision_recall_curve(y_test, y_pred)
+pr, rc, thresholds = precision_recall_curve(y_test, y_pred_rt[:, 1])
+print("roc_auc for class 1:", roc_auc_score(y_test,y_pred_rt[:, 1]))
 # Figure 1: precision, recall vs thresholds
 plt.figure(1)
 plt.plot(thresholds, pr[1:])
@@ -73,6 +74,7 @@ print(classification_report(y_test, y_pred > crossover_cutoff))
 m_confusion_test = confusion_matrix(y_test, y_pred > crossover_cutoff)
 print(pd.DataFrame(data = m_confusion_test, columns = ['Predicted 0', 'Predicted 1'],
             index = ['Actual 0', 'Actual 1']))
+
 
 # ==================================================================================================================
 plt.show()
